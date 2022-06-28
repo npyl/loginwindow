0c7d3ee809c7d2fc549f701e1d81431cd1be007c
npyl <pylarinosnick@gmail.com>
2022-06-28 15:17:05 +0300

    Initial Commit

---
+++ loginwindow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  loginwindow
+//
+//  Created by Nickolas Pylarinos Stamatelatos on 28/06/2022.
+//  Copyright © 2022 Nickolas Pylarinos. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ loginwindow/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  loginwindow
+//
+//  Created by Nickolas Pylarinos Stamatelatos on 28/06/2022.
+//  Copyright © 2022 Nickolas Pylarinos. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
